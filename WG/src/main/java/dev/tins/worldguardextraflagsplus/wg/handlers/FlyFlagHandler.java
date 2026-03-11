893435de594e65f23969c15ddecd0c37d6c17a77
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2026-03-11 18:46:22 +0000

    Rewrite Fly Flag Handler

--- WG/src/main/java/dev/tins/worldguardextraflagsplus/wg/handlers/FlyFlagHandler.java
+++ WG/src/main/java/dev/tins/worldguardextraflagsplus/wg/handlers/FlyFlagHandler.java
@@ -10,253 +10,133 @@ import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-
 import dev.tins.worldguardextraflagsplus.wg.WorldGuardUtils;
-
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.StateFlag.State;
 import com.sk89q.worldguard.session.MoveType;
 import com.sk89q.worldguard.session.Session;
-
 import lombok.Getter;
 import dev.tins.worldguardextraflagsplus.flags.Flags;
+import java.lang.reflect.Method;
 
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.session.Session;
-import com.sk89q.worldguard.session.SessionManager;
-import com.sk89q.worldguard.LocalPlayer;
+public class FlyFlagHandler extends FlagValueChangeHandler<State> {
 
-import org.bukkit.entity.Player;
+	public static Factory FACTORY() { return new Factory(); }
 
-
-public class FlyFlagHandler extends FlagValueChangeHandler<State>
-{
-	public static void recheckPlayer(Player player) {
-
-	LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
-
-	SessionManager manager = WorldGuard.getInstance()
-			.getPlatform()
-			.getSessionManager();
-
-	Session session = manager.get(localPlayer);
-
-	if (session != null) {
-		session.testMoveTo(localPlayer, localPlayer.getLocation(), MoveType.MOVE);
-	}
-};
-	public static final Factory FACTORY()
-	{
-		return new Factory();
-	}
-	
-    public static class Factory extends Handler.Factory<FlyFlagHandler>
-    {
+	public static class Factory extends Handler.Factory<FlyFlagHandler> {
 		@Override
-        public FlyFlagHandler create(Session session)
-        {
-            return new FlyFlagHandler(session);
-        }
-    }
-	private  boolean isFlyEssentialsX()
-	{
-		try
-		{
-			Class<?> configClass = Class.forName("dev.tins.worldguardextraflagsplus.Config");
-			java.lang.reflect.Method getMethod = configClass.getMethod("isFlyEssentialsX");
-			return (Boolean) getMethod.invoke(null);
-		}
-		catch (Exception e)
-		{
-			return false;
+		public FlyFlagHandler create(Session session) { return new FlyFlagHandler(session); }
+	}
+
+	@Getter private Boolean currentValue;
+	@Setter private Boolean originalFly;
+
+	private static Object essentialsPlugin;
+	private static Method getUserMethod;
+	private static Method setFlyMethod;
+	private static boolean initialized = false;
+
+	private static void setupEssentials() {
+		if (initialized) return;
+		initialized = true;
+		try {
+			Plugin plugin = Bukkit.getPluginManager().getPlugin("Essentials");
+			if (plugin == null) plugin = Bukkit.getPluginManager().getPlugin("EssentialsX");
+
+			if (plugin != null) {
+				essentialsPlugin = plugin;
+				getUserMethod = plugin.getClass().getMethod("getUser", Player.class);
+
+				// We search the User class for the best available flight method
+				// This avoids the "NoSuchMethod" error by checking what actually exists
+				Class<?> userClass = Class.forName("com.earth2me.essentials.User");
+
+				String[] candidates = {"setAllowFlight", "setFly", "setFlying"};
+				for (String name : candidates) {
+					try {
+						// Try primitive boolean first
+						setFlyMethod = userClass.getMethod(name, boolean.class);
+						break;
+					} catch (NoSuchMethodException e) {
+						try {
+							// Try Object Boolean second
+							setFlyMethod = userClass.getMethod(name, Boolean.class);
+							break;
+						} catch (NoSuchMethodException ignored) {}
+					}
+				}
+
+				if (setFlyMethod != null) {
+					Bukkit.getLogger().info("[FlyFlagHandler] Hooked EssentialsX via: " + setFlyMethod.getName());
+				}
+			}
+		} catch (Exception e) {
+			Bukkit.getLogger().warning("[FlyFlagHandler] EssentialsX hook failed: " + e.getMessage());
 		}
 	}
 
-    @Getter private Boolean currentValue;
-    @Setter private Boolean originalFly;
-
-    private static final ConcurrentHashMap<UUID, Boolean> originalWorldGuardFlyCache = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<UUID, Boolean> originalEssentialsFlyCache = new ConcurrentHashMap<>();
-    private static Object essentialsAPI;
-    private static boolean essentialsChecked = false;
-
-    private static void initEssentialsAPI()
-    {
-    	if (essentialsChecked)
-    	{
-    		return;
-    	}
-
-    	essentialsChecked = true;
-
-    	try
-    	{
-    		Plugin essentialsPlugin = Bukkit.getPluginManager().getPlugin("Essentials");
-    		if (essentialsPlugin == null)
-    		{
-    			essentialsPlugin = Bukkit.getPluginManager().getPlugin("EssentialsX");
-    		}
-
-    		if (essentialsPlugin != null)
-    		{
-    			Class<?> iEssentialsClass = Class.forName("net.ess3.api.IEssentials");
-    			if (iEssentialsClass.isInstance(essentialsPlugin))
-    			{
-    				essentialsAPI = essentialsPlugin;
-    			}
-    		}
-    	}
-    	catch (ClassNotFoundException | NoClassDefFoundError e)
-    	{
-    		essentialsAPI = null;
-    	}
-    	catch (Exception e)
-    	{
-    		essentialsAPI = null;
-    	}
-    }
-
-    private static boolean isEssentialsAvailable()
-    {
-    	initEssentialsAPI();
-    	return essentialsAPI != null;
-    }
-
-	protected FlyFlagHandler(Session session)
-	{
+	protected FlyFlagHandler(Session session) {
 		super(session, Flags.FLY);
 	}
 
 	@Override
-	protected void onInitialValue(LocalPlayer player, ApplicableRegionSet set, State value)
-	{
+	protected void onInitialValue(LocalPlayer player, ApplicableRegionSet set, State value) {
 		this.handleValue(player, player.getWorld(), value);
 	}
 
 	@Override
-	protected boolean onSetValue(LocalPlayer player, Location from, Location to, ApplicableRegionSet toSet, State currentValue, State lastValue, MoveType moveType)
-	{
+	protected boolean onSetValue(LocalPlayer player, Location from, Location to, ApplicableRegionSet toSet, State currentValue, State lastValue, MoveType moveType) {
 		this.handleValue(player, (World) to.getExtent(), currentValue);
 		return true;
 	}
 
 	@Override
-	protected boolean onAbsentValue(LocalPlayer player, Location from, Location to, ApplicableRegionSet toSet, State lastValue, MoveType moveType)
-	{
+	protected boolean onAbsentValue(LocalPlayer player, Location from, Location to, ApplicableRegionSet toSet, State lastValue, MoveType moveType) {
 		this.handleValue(player, (World) to.getExtent(), null);
 		return true;
 	}
-	
-	private void handleValue(LocalPlayer player, World world, State state)
-	{
+
+	private void handleValue(LocalPlayer player, World world, State state) {
 		Player bukkitPlayer = ((BukkitPlayer) player).getPlayer();
-
-		// Don't schedule tasks during shutdown
-		if (!WorldGuardUtils.isPluginEnabled() || !bukkitPlayer.isOnline())
-		{
-			return;
-		}
-
-		UUID playerUUID = bukkitPlayer.getUniqueId();
+		if (bukkitPlayer == null) return;
 
 		WorldGuardUtils.getScheduler().runAtEntity(bukkitPlayer, task -> {
-			if (!this.getSession().getManager().hasBypass(player, world) && state != null)
-			{
-				boolean value = state == State.ALLOW;
-
-				// Disable EssentialsX fly mode if it's enabled
-				if (isEssentialsAvailable() && isFlyEssentialsX() == true && essentialsAPI != null)
-				{
-					try
-					{
-						Class<?> iEssentialsClass = Class.forName("net.ess3.api.IEssentials");
-						java.lang.reflect.Method getUserMethod = iEssentialsClass.getMethod("getUser", Player.class);
-						Object essentialsUser = getUserMethod.invoke(essentialsAPI, bukkitPlayer);
-
-						if (essentialsUser != null)
-						{
-							Class<?> userClass = Class.forName("com.earth2me.essentials.User");
-							if (userClass.isInstance(essentialsUser))
-							{
-								java.lang.reflect.Method getFlyMethod = userClass.getMethod("getFly");
-								Boolean essentialsFlyEnabled = (Boolean) getFlyMethod.invoke(essentialsUser);
-
-								if (essentialsFlyEnabled != null && essentialsFlyEnabled)
-								{
-									if (!originalEssentialsFlyCache.containsKey(playerUUID))
-									{
-										originalEssentialsFlyCache.put(playerUUID, true);
-									}
-
-									java.lang.reflect.Method setFlyMethod = userClass.getMethod("setFly", boolean.class);
-									setFlyMethod.invoke(essentialsUser, false);
-								}
-							}
-						}
-					}
-					catch (Exception e)
-					{
-						// Silently ignore - EssentialsX may not be available or player doesn't exist
-					}
+			if (state == State.ALLOW && !this.getSession().getManager().hasBypass(player, world)) {
+				if (this.currentValue == null || !this.currentValue) {
+					this.originalFly = bukkitPlayer.getAllowFlight();
+					setEssentialsFly(bukkitPlayer, true);
+					bukkitPlayer.setAllowFlight(true);
+					this.currentValue = true;
 				}
-
-				if (bukkitPlayer.getAllowFlight() != value)
-				{
-					if (this.originalFly == null)
-					{
-						this.originalFly = bukkitPlayer.getAllowFlight();
-					}
-
-					bukkitPlayer.setAllowFlight(value);
-				}
-
-				this.currentValue = value;
 			}
-			else
-			{
-				// Restore EssentialsX fly mode
-				Boolean cachedEssentialsFly = originalEssentialsFlyCache.get(playerUUID);
-				if (cachedEssentialsFly != null && cachedEssentialsFly)
-				{
-					if (isEssentialsAvailable() && isFlyEssentialsX() == true && essentialsAPI != null)
-					{
-						try
-						{
-							Class<?> iEssentialsClass = Class.forName("net.ess3.api.IEssentials");
-							java.lang.reflect.Method getUserMethod = iEssentialsClass.getMethod("getUser", Player.class);
-							Object essentialsUser = getUserMethod.invoke(essentialsAPI, bukkitPlayer);
+			else if (this.currentValue != null || this.originalFly != null) {
+				// Use a direct plugin lookup for the scheduler
+				Plugin main = Bukkit.getPluginManager().getPlugin("WorldGuardExtraFlagsPlus");
+				if (main != null) {
+					Bukkit.getScheduler().runTaskLater(main, () -> {
+						setEssentialsFly(bukkitPlayer, false);
 
-							if (essentialsUser != null)
-							{
-								Class<?> userClass = Class.forName("com.earth2me.essentials.User");
-								if (userClass.isInstance(essentialsUser))
-								{
-									java.lang.reflect.Method setFlyMethod = userClass.getMethod("setFly", boolean.class);
-									setFlyMethod.invoke(essentialsUser, true);
-								}
-							}
-						}
-						catch (Exception e)
-						{
-							// Silently ignore
-						}
-					}
+						boolean restoreValue = (this.originalFly != null) ? this.originalFly : false;
+						bukkitPlayer.setAllowFlight(restoreValue);
+						if (!restoreValue) bukkitPlayer.setFlying(false);
+
+						this.originalFly = null;
+						this.currentValue = null;
+					}, 2L);
 				}
-
-				if (this.originalFly != null)
-				{
-					bukkitPlayer.setAllowFlight(this.originalFly);
-
-					this.originalFly = null;
-				}
-
-				this.currentValue = null;
-				originalEssentialsFlyCache.remove(playerUUID);
 			}
 		});
 	}
-	
+
+	private void setEssentialsFly(Player player, boolean state) {
+		setupEssentials();
+		if (essentialsPlugin == null || getUserMethod == null || setFlyMethod == null) return;
+
+		try {
+			Object user = getUserMethod.invoke(essentialsPlugin, player);
+			if (user != null) {
+				setFlyMethod.invoke(user, state);
+			}
+		} catch (Exception ignored) {}
+	}
 }
